ee9035752d1cd6430bc2a9dfd976f7542f4c1a0c
jermsinarocket <jermsinarocket@gmail.com>
2020-08-30 14:57:15 +0800

    Remake with new App Name

--- SEProject/Enso/Enso/wsgi.py
+++ SEProject/Enso/Enso/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tempproject project.
+WSGI config for Enso project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tempproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Enso.settings')
 
 application = get_wsgi_application()
